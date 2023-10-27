693c43187d2d3f154c5a044d0e830ad5e8ddffba
Tayler <taylerblevins@gmail.com>
2023-10-27 13:32:15 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names){
+  const messages = [];
+  for (let i=0; i<names.length;i++){
+    console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+  }
+  return messages;
+}
+function countDown(n){
+  while (n>=0){
+    console.log(n--)
+  }
+}
